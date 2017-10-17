189d61a1a7ee3424427148bf8da510124b07df74
igor <t-igor@ersl.ie>
2017-10-18 01:46:28 +0300

    fix pattern content

--- Project2015To2017/FileTransformation.cs
+++ Project2015To2017/FileTransformation.cs
@@ -9,142 +9,143 @@ using System.Linq;
 
 namespace Project2015To2017
 {
-    internal sealed class FileTransformation : ITransformation
-    {
-        private static readonly IReadOnlyList<string> itemsToProject = new[]
-        {
-            "None",
-            "Content",
-            "AdditionalFiles",
-            "CodeAnalysisDictionary",
-            "ApplicationDefinition",
-            "Page",
-            "Resource",
-            "SplashScreen",
-            "DesignData",
-            "DesignDataWithDesignTimeCreatableTypes",
-            "EntityDeploy",
-            "XamlAppDef"
-        };
+	internal sealed class FileTransformation : ITransformation
+	{
+		private static readonly IReadOnlyList<string> itemsToProject = new[]
+		{
+			"None",
+			"Content",
+			"AdditionalFiles",
+			"CodeAnalysisDictionary",
+			"ApplicationDefinition",
+			"Page",
+			"Resource",
+			"SplashScreen",
+			"DesignData",
+			"DesignDataWithDesignTimeCreatableTypes",
+			"EntityDeploy",
+			"XamlAppDef"
+		};
 
-        public Task TransformAsync(XDocument projectFile, DirectoryInfo projectFolder, Project definition)
-        {
-            XNamespace nsSys = "http://schemas.microsoft.com/developer/msbuild/2003";
-            var itemGroups = projectFile
-                .Element(nsSys + "Project")
-                .Elements(nsSys + "ItemGroup");
+		public Task TransformAsync(XDocument projectFile, DirectoryInfo projectFolder, Project definition)
+		{
+			XNamespace nsSys = "http://schemas.microsoft.com/developer/msbuild/2003";
+			var itemGroups = projectFile
+				.Element(nsSys + "Project")
+				.Elements(nsSys + "ItemGroup");
 
-            var compileManualIncludes = FindNonWildcardMatchedFiles(projectFolder, itemGroups, "*.cs", nsSys + "Compile");
-            var otherIncludes = itemsToProject.SelectMany(x => itemGroups.Elements(nsSys + x));
+			var compileManualIncludes = FindNonWildcardMatchedFiles(projectFolder, itemGroups, "*.cs", nsSys + "Compile");
+			var otherIncludes = itemsToProject.SelectMany(x => itemGroups.Elements(nsSys + x));
 
 			var otherNodes = compileManualIncludes
-		        .Where(i => !i.Elements().Any(IsDependent))
+				.Where(i => !i.Elements().Any(IsDependent))
 				.RenameAttribute("Include", "Update")
 				.ToList();
 
-	        var generatedNodes = compileManualIncludes
-		        .Where(i => i.Elements().SingleOrDefault(IsDependent) != null)
-				.Select(i => i.Elements().SingleOrDefault(IsDependent));
-
-	        if (generatedNodes?.Any() == true)
-				otherNodes.Add(XElementExtensions.CreateNode(
-					"Compile", 
-					new [] { new XAttribute("Update", "*.generated.cs") }, 
-					new [] { XElementExtensions.CreateNode("DependentUpon", value: ".tt") }));
+			var generatedNodes = compileManualIncludes
+				.Where(i => i.Elements().SingleOrDefault(IsDependent) != null)
+				.Select(i => i.Elements().SingleOrDefault(IsDependent))
+				.GroupBy(x => x.Value)
+				.Select(e => XElementExtensions.CreateNode(
+					"Compile",
+					new[] { new XAttribute("Update", "*.generated.cs") },
+					new[] { e.First() }))
+				.ToList();
 
 			definition.ItemsToInclude =
 				otherIncludes
 					.RenameAttribute("Include", "Update")
 					.RenameNode("Content", "None")
 				.Concat(otherNodes)
+				.Concat(generatedNodes)
 				.ToList();
 
 			return Task.CompletedTask;
-        }
+		}
 
-	    private bool IsDependent(XElement node)
-	    {
-		    return node.Name.LocalName == "DependentUpon" && node.Value.Trim().EndsWith(".tt");
-	    }
+		private bool IsDependent(XElement node)
+		{
+			return node.Name.LocalName == "DependentUpon" && node.Value.Trim().EndsWith(".tt");
+		}
 
 		private static List<XElement> FindNonWildcardMatchedFiles(
-            DirectoryInfo projectFolder, 
-            IEnumerable<XElement> itemGroups, 
-            string wildcard, 
-            XName elementName)
-        {
-            var manualIncludes = new List<XElement>();
-            var filesMatchingWildcard = new List<string>();
-            foreach (var compiledFile in itemGroups.Elements(elementName))
-            {
-                var includeAttribute = compiledFile.Attribute("Include");
-                if (includeAttribute != null)
-                {
-                    if (!Path.GetFullPath(Path.Combine(projectFolder.FullName, includeAttribute.Value)).StartsWith(projectFolder.FullName))
-                    {
-                        Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile.ToString()}.");
-                        manualIncludes.Add(compiledFile);
-                    }
-                    else if (compiledFile.Attributes().Count() != 1)
-                    {
-                        Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile.ToString()}.");
-                        manualIncludes.Add(compiledFile);
-                    }
-                    else if (compiledFile.Elements().Count() != 0)
-                    {
-                        var dependentUpon = compiledFile.Element(elementName.Namespace + "DependentUpon");
-                        if (dependentUpon != null && dependentUpon.Value.EndsWith(".resx", StringComparison.OrdinalIgnoreCase))
-                        {
-                            // resx generated code file
-                            manualIncludes.Add(new XElement(
-                                "Compile",
-                                new XAttribute("Update", includeAttribute.Value),
-                                new XElement("DependentUpon", dependentUpon.Value)));
+			DirectoryInfo projectFolder,
+			IEnumerable<XElement> itemGroups,
+			string wildcard,
+			XName elementName)
+		{
+			var manualIncludes = new List<XElement>();
+			var filesMatchingWildcard = new List<string>();
+			foreach (var compiledFile in itemGroups.Elements(elementName))
+			{
+				var includeAttribute = compiledFile.Attribute("Include");
+				if (includeAttribute != null)
+				{
+					if (!Path.GetFullPath(Path.Combine(projectFolder.FullName, includeAttribute.Value)).StartsWith(projectFolder.FullName))
+					{
+						Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile.ToString()}.");
+						manualIncludes.Add(compiledFile);
+					}
+					else if (compiledFile.Attributes().Count() != 1)
+					{
+						Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile.ToString()}.");
+						manualIncludes.Add(compiledFile);
+					}
+					else if (compiledFile.Elements().Count() != 0)
+					{
+						var dependentUpon = compiledFile.Element(elementName.Namespace + "DependentUpon");
+						if (dependentUpon != null && dependentUpon.Value.EndsWith(".resx", StringComparison.OrdinalIgnoreCase))
+						{
+							// resx generated code file
+							manualIncludes.Add(new XElement(
+								"Compile",
+								new XAttribute("Update", includeAttribute.Value),
+								new XElement("DependentUpon", dependentUpon.Value)));
 
-                            filesMatchingWildcard.Add(includeAttribute.Value);
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile.ToString()}.");
-                            manualIncludes.Add(compiledFile);
-                        }
-                    }
-                    else
-                    {
-                        filesMatchingWildcard.Add(includeAttribute.Value);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Compile found with no include, full node {compiledFile.ToString()}.");
-                }
-            }
+							filesMatchingWildcard.Add(includeAttribute.Value);
+						}
+						else
+						{
+							Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile.ToString()}.");
+							manualIncludes.Add(compiledFile);
+						}
+					}
+					else
+					{
+						filesMatchingWildcard.Add(includeAttribute.Value);
+					}
+				}
+				else
+				{
+					Console.WriteLine($"Compile found with no include, full node {compiledFile.ToString()}.");
+				}
+			}
 
-            var filesInFolder = projectFolder.EnumerateFiles(wildcard, SearchOption.AllDirectories).Select(x => x.FullName).ToArray();
-            var knownFullPaths = manualIncludes
-                .Select(x => x.Attribute("Include")?.Value)
-                .Where(x => x != null)
-                .Concat(filesMatchingWildcard)
-                .Select(x => Path.GetFullPath(Path.Combine(projectFolder.FullName, x)))
-                .ToArray();
+			var filesInFolder = projectFolder.EnumerateFiles(wildcard, SearchOption.AllDirectories).Select(x => x.FullName).ToArray();
+			var knownFullPaths = manualIncludes
+				.Select(x => x.Attribute("Include")?.Value)
+				.Where(x => x != null)
+				.Concat(filesMatchingWildcard)
+				.Select(x => Path.GetFullPath(Path.Combine(projectFolder.FullName, x)))
+				.ToArray();
 
-            foreach (var nonListedFile in filesInFolder.Except(knownFullPaths))
-            {
-                if (nonListedFile.StartsWith(Path.Combine(projectFolder.FullName + "\\obj"), StringComparison.OrdinalIgnoreCase))
-                {
-                    // skip the generated files in obj
-                    continue;
-                }
+			foreach (var nonListedFile in filesInFolder.Except(knownFullPaths))
+			{
+				if (nonListedFile.StartsWith(Path.Combine(projectFolder.FullName + "\\obj"), StringComparison.OrdinalIgnoreCase))
+				{
+					// skip the generated files in obj
+					continue;
+				}
 
-                Console.WriteLine($"File found which was not included, consider removing {nonListedFile}.");
-            }
+				Console.WriteLine($"File found which was not included, consider removing {nonListedFile}.");
+			}
 
-            foreach (var fileNotOnDisk in knownFullPaths.Except(filesInFolder).Where(x => x.StartsWith(projectFolder.FullName, StringComparison.OrdinalIgnoreCase)))
-            {
-                Console.WriteLine($"File was included but is not on disk: {fileNotOnDisk}.");
-            }
+			foreach (var fileNotOnDisk in knownFullPaths.Except(filesInFolder).Where(x => x.StartsWith(projectFolder.FullName, StringComparison.OrdinalIgnoreCase)))
+			{
+				Console.WriteLine($"File was included but is not on disk: {fileNotOnDisk}.");
+			}
 
-            return manualIncludes;
-        }
-    }
+			return manualIncludes;
+		}
+	}
 }
